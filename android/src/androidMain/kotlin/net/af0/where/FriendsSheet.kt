86ea6b72a04083747542af5e1527b95ae40fd320
Dan Margolis <dan@af0.net>
2026-05-16 15:06:15 +0200

    Fix syntax error in Android FriendsSheet.kt

--- android/src/androidMain/kotlin/net/af0/where/FriendsSheet.kt
+++ android/src/androidMain/kotlin/net/af0/where/FriendsSheet.kt
@@ -101,8 +101,6 @@ fun FriendsSheet(
                     },
                     modifier = Modifier.weight(1f),
                 ) {
-
-                ) {
                     Icon(Icons.Default.QrCodeScanner, contentDescription = null, modifier = Modifier.size(16.dp))
                     Text(" " + stringResource(MR.strings.scan))
                 }
