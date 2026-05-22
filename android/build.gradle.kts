d0e704846fc0e0cdc6435eb64590724083a50e06
Dan Margolis <dan@af0.net>
2026-05-22 23:04:54 +0200

    Update versions and update fastlane

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "net.af0.where"
         minSdk = 26
         targetSdk = 35
-        versionCode = 78
-        versionName = "2026.05.21.1"
+        versionCode = 79
+        versionName = "2026.05.22.1"
         manifestPlaceholders["MAPS_API_KEY"] = localProperties.getProperty("MAPS_API_KEY") ?: System.getenv("MAPS_API_KEY") ?: ""
     }
 
