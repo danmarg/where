0eaaca21cfe771a0a0546504cd0b119e8595cd7b
Dan Margolis <dan@af0.net>
2026-06-12 22:54:18 +0200

    Increment versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "net.af0.where"
         minSdk = 26
         targetSdk = 35
-        versionCode = 95
-        versionName = "2026.06.10.1"
+        versionCode = 96
+        versionName = "2026.06.12.1"
         manifestPlaceholders["MAPS_API_KEY"] = localProperties.getProperty("MAPS_API_KEY") ?: System.getenv("MAPS_API_KEY") ?: ""
     }
 
