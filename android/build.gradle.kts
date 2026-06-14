a41872640408fb2f3ec12af15ca2ca1e146e07b1
Dan Margolis <dan@af0.net>
2026-06-14 08:55:17 +0200

    Version bump

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -61,8 +61,8 @@ android {
         applicationId = "net.af0.where"
         minSdk = 26
         targetSdk = 35
-        versionCode = 96
-        versionName = "2026.06.12.1"
+        versionCode = 97
+        versionName = "2026.06.14.1"
         manifestPlaceholders["MAPS_API_KEY"] = localProperties.getProperty("MAPS_API_KEY") ?: System.getenv("MAPS_API_KEY") ?: ""
     }
 
