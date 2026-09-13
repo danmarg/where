58bfaed420cb31d2369f880a56e08194b7d93ebe
Dan Margolis <dan@af0.net>
2026-09-13 19:28:35 +0000

    Bump Android version to 2026.09.13.5 (137)

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -63,8 +63,8 @@ android {
         applicationId = "net.af0.where"
         minSdk = 26
         targetSdk = 36
-        versionCode = 136
-        versionName = "2026.09.13.4"
+        versionCode = 137
+        versionName = "2026.09.13.5"
 
         // JNA (a transitive dep of the libsodium bindings) ships dispatch stubs for
         // legacy ABIs no Android device has used in years (armeabi, mips, mips64),
